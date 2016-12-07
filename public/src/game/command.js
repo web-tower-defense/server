4bf5b6d0b7ebc0e82518e4d0a1f3d24bc301d788
ryanpatiency <ryanpatiency@gmail.com>
2016-12-07 21:43:28 +0800

    delete typescript part

--- public/src/game/command.js
+++
@@ -1,4 +0,0 @@
-function Command(selected,target){
-	this.selected=selected;
-	this.target=target;
-}
--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/phaser/index.d.ts" />
-/// <reference path="globals/socket.io-client/index.d.ts" />
-/// <reference path="globals/socket.io/index.d.ts" />
